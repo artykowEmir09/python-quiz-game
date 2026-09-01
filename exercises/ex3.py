7b79e49afc445743840b93e6b1260fde711f765b
artykowEmir09 <artykowemir09@gmail.com>
2026-09-01 09:38:11 +0800

    Clean repository for quiz game

--- exercises/ex3.py
+++
@@ -1,8 +0,0 @@
-numbers = [[1,2,3],
-           [4,5,6],
-           [7,8,9],
-           ["*",0,"#"]]
-for x in numbers:
-    for y in x :
-        print(y, end =" ")
-    print()
--- exercises/exercise2.py
+++
@@ -1,9 +0,0 @@
-groceries = [["apple","orange","banana"],
-            ["potato","karrot","tomato"],
-            ["chicken","lamb","fish"]]
-
-for collection in groceries:
-    
-    for food in collection:
-        print(food, end=" ")
-    print()
