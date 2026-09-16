8e900ae8ab0afaf058adeaa554827fdab0a50c40
artykowEmir09 <artykowemir09@gmail.com>
2026-09-16 13:07:41 +0800

    Add new project

--- classess/magic_method.py
+++ classess/magic_method.py
@@ -5,10 +5,12 @@ class Book:
         self.num_pages = num_pages
     def __str__(self):
         return f"'{self.title}' by {self.author}"
+    def __eq__(self, other):
+        return self.title == other.title and self.author == other.author
 
 
 
-book1 = Book("Atomic Habits", "J.Clear", 223)
+book1 = Book("No More Mr. Nice Guy", "Robert A. Glover", 223)
 book2 = Book("No More Mr. Nice Guy", "Robert A. Glover", 208)
-book1 = Book("How to Win Friends and Influence People", " Dale Carnegie", 192)
-print(book1)
+book3 = Book("How to Win Friends and Influence People", " Dale Carnegie", 192)
+print(book2 == book1)
