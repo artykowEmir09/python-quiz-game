d35338a5a87ae419b062ad9f599cfc1ed68aceba
artykowEmir09 <artykowemir09@gmail.com>
2026-09-22 23:09:15 +0800

    Update project

--- projects/new.py
+++ projects/new.py
@@ -31,3 +31,22 @@ def show_expenses():
 
 
 while True:
+    print("\n===== Expense Tracker =====")
+    print("1. Add expense")
+    print("2. Show expenses")
+    print("3. Exit")
+
+    choice = input("Choose an option: ")
+
+    if choice == "1":
+        add_expense()
+
+    elif choice == "2":
+        show_expenses()
+
+    elif choice == "3":
+        print("Goodbye!")
+        break
+
+    else:
+        print("Invalid choice.")
